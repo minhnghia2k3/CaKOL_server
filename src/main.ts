73feb299f44a2022b16dcbb91ff72133bb016706
minhnghia2k3 <BrozennVN@gmail.com>
2024-04-22 15:05:28 +0700

    modify cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.CLIENT,
+    origin: [process.env.CLIENT, 'http://localhost:3000'],
     credentials: true,
   });
   app.useGlobalPipes(
